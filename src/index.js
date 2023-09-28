e05c36fca33a8bc1bad33c56e4253f2578e12ede
Nadya Penkrat <nadya.penkrat@gmail.com>
2023-09-28 14:21:44 +0300

    feat: add practice code-basics js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for(let i = 0; i < expr.length; i += 10) {
+        let morse = '';
+        let subs = expr.slice(i, i+10);
+        if(subs === '**********') {result += ' '; continue;}
+        for(let j = 0; j < 10; j += 2) {
+            let subTwo = subs.slice(j, j+2);
+            if(subTwo === '10') morse += '.';
+            else if(subTwo === '11') morse += '-';
+        }
+        result += MORSE_TABLE[morse];
+    }
+    return result;
 }
 
 module.exports = {
